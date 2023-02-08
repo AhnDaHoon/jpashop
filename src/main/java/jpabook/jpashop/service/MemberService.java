96cd967822c5e137cd5c7e4983940db45f760137
DESKTOP-BSAFK84\topvel <psycn@naver.com>
2023-02-08 16:39:24 +0900

    jpa 실전편1 (2023-02-08)

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+
     /**
      * 회원가입
      */
@@ -36,10 +37,12 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
+
         return memberRepository.findAll();
     }
 
     public Member findOne(Long memberId) {
+
         return memberRepository.findOne(memberId);
     }
 }
